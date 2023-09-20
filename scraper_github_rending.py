2315ffc98561f95cc372627ef2f238f1352916e5
caoyanfei <caoyanfei@dycjr.com>
2023-09-20 23:46:12 +0800

    add init script

---
+++ scraper_github_rending.py
@@ -0,0 +1,93 @@
+# coding:utf-8
+
+import datetime
+import codecs
+import requests
+import os
+import time
+from pyquery import PyQuery as pq
+
+
+def git_add_commit_push(date, filename):
+    cmd_git_add = 'git add {filename}'.format(filename=filename)
+    cmd_git_commit = 'git commit -m "{date}"'.format(date=date)
+    cmd_git_push = 'git push -u origin master'
+
+    os.system(cmd_git_add)
+    os.system(cmd_git_commit)
+    os.system(cmd_git_push)
+
+
+def createMarkdown(date, filename):
+    # 获取文件所在的目录路径
+    directory_path = os.path.dirname(filename)
+
+    # 检查目录是否存在，如果不存在则创建
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    with open(filename, 'w') as f:
+        f.write("## " + date + "\n")
+
+
+def scrape(language, filename):
+    HEADERS = {
+        'User-Agent'		: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+        'Accept'			: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding'	: 'gzip,deflate,sdch',
+        'Accept-Language'	: 'zh-CN,zh;q=0.8'
+    }
+
+    if language == 'default':
+        url = 'https://github.com/trending'
+    else:
+        url = 'https://github.com/trending/{language}'.format(language=language)
+    r = requests.get(url, headers=HEADERS)
+    assert r.status_code == 200
+    
+    d = pq(r.content)
+    items = d('div.Box article.Box-row')
+
+    # codecs to solve the problem utf-8 codec like chinese
+    with codecs.open(filename, "a", "utf-8") as f:
+        f.write('\n#### {language}\n'.format(language=language))
+
+        for item in items:
+            i = pq(item)
+            title = i(".lh-condensed a").text()
+            owner = i(".lh-condensed span.text-normal").text()
+            description = i("p.col-9").text()
+            url = i(".lh-condensed a").attr("href")
+            url = "https://github.com" + url
+            # ownerImg = i("p.repo-list-meta a img").attr("src")
+            # print(ownerImg)
+            f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
+
+
+def job():
+    strdate = datetime.datetime.now().strftime('%Y-%m-%d')
+    strYear = datetime.datetime.now().strftime('%Y')
+    strMonth = datetime.datetime.now().strftime('%m')
+    filename = 'github/{year}/{month}/{date}.md'.format(date=strdate,year=strYear, month=strMonth)
+
+    # create markdown file
+    createMarkdown(strdate, filename)
+
+    # write markdown
+    scrape('all', filename)
+    scrape('default', filename)
+    scrape('python', filename)
+    scrape('go', filename)
+    scrape('java', filename)
+    scrape('javascript', filename)
+    scrape('typescript', filename)
+    scrape('vue', filename)
+    
+    
+
+    # git add commit push
+    # git_add_commit_push(strdate, filename)
+
+
+if __name__ == '__main__':
+    job()
