fbc8f039e155d1a25ecf0d86a356b808d6a26465
Cao Yanfei <github@uyhnm.cn>
2025-03-27 13:47:10 +0800

    Update scraper_github_rending_week.py
    增加中文

--- scraper_github_rending_week.py
+++ scraper_github_rending_week.py
@@ -63,6 +63,38 @@ def scrape(language, filename):
             # print(ownerImg)
             f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
 
+def scrape2(language, filename):
+    HEADERS = {
+        'User-Agent'		: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+        'Accept'			: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding'	: 'gzip,deflate,sdch',
+        'Accept-Language'	: 'zh-CN,zh;q=0.8'
+    }
+
+    if language == 'default':
+        url = 'https://github.com/trending?since=weekly&spoken_language_code=zh'
+    else:
+        url = 'https://github.com/trending/{language}?since=weekly&spoken_language_code=zh'.format(language=language)
+    r = requests.get(url, headers=HEADERS)
+    assert r.status_code == 200
+    
+    d = pq(r.content)
+    items = d('div.Box article.Box-row')
+
+    # codecs to solve the problem utf-8 codec like chinese
+    with codecs.open(filename, "a", "utf-8") as f:
+        f.write('\n#### {language}\n'.format(language=language))
+
+        for item in items:
+            i = pq(item)
+            title = i(".lh-condensed a").text()
+            owner = i(".lh-condensed span.text-normal").text()
+            description = i("p.col-9").text()
+            url = i(".lh-condensed a").attr("href")
+            url = "https://github.com" + url
+            # ownerImg = i("p.repo-list-meta a img").attr("src")
+            # print(ownerImg)
+            f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
 
 def job():
     strdate = datetime.datetime.now().strftime('%Y-%m-%d')
@@ -75,6 +107,7 @@ def job():
     createMarkdown(strdate, filename)
 
     # write markdown
+    scrape2('all', filename)
     scrape('all', filename)
     scrape('default', filename)
     scrape('python', filename)
